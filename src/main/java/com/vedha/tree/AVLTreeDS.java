a38e275ae865b18f24a9b8b64fac282e021a2bc0
Vedha <sarath1242000@gmail.com>
2025-01-20 19:56:11 +0530

    Binary Tree

--- src/main/java/com/vedha/tree/AVLTreeDS.java
+++ src/main/java/com/vedha/tree/AVLTreeDS.java
@@ -1,11 +1,11 @@
 package com.vedha.tree;
 
 
-public class BinarySearchTreeDS<T> {
+public class AVLTreeDS<T> {
 
     Node root;
 
-    public BinarySearchTreeDS(T initialData) {
+    public AVLTreeDS(T initialData) {
 
         this.root = new Node(initialData);
     }
@@ -14,20 +14,23 @@ public class BinarySearchTreeDS<T> {
 
         T data;
         Node left, right;
+        int height;
 
         public Node(T data) {
             this.data = data;
             this.left = null;
             this.right = null;
+            this.height = 0;
         }
 
         @Override
         public String toString() {
             return "Node{" +
-                   "data=" + data +
-                   ", left=" + left +
-                   ", right=" + right +
-                   '}';
+                    "data=" + data +
+                    ", left=" + left +
+                    ", right=" + right +
+                    ", height=" + height +
+                    '}';
         }
     }
 
@@ -40,7 +43,7 @@ public class BinarySearchTreeDS<T> {
         insertMethodCount = 0;
         System.out.println("Inserting: " + data);
 
-        insert(this.root, data);
+        this.root = insert(this.root, data);
 
         System.out.println("insertCount: " + insertCount);
         System.out.println("insertMethodCount: " + insertMethodCount);
@@ -60,12 +63,95 @@ public class BinarySearchTreeDS<T> {
         if ((int) root.data > (int) data) {
 
             root.left = insert(root.left, data);
-        } else {
+        } else if ((int) root.data < (int) data) {
 
             root.right = insert(root.right, data);
+        } else {
+
+            return root;
+        }
+
+        root.height = max(getNodeHeight(root.left), getNodeHeight(root.right)) + 1;
+
+        // Get the balance factor of this ancestor node to check whether this node became unbalanced or not
+        // If this node becomes unbalanced, then there are 4 cases, Left-Left Case, Left-Right Case, Right-Right Case, Right-Left Case
+        int balanceFactor = getBalanceFactor(root);
+        // Left-Left Case
+        if (balanceFactor > 1 && (int) root.left.data > (int) data) {
+
+            return rightRotate(root);
+        }
+
+        // Left-Right Case
+        if (balanceFactor > 1 && (int) root.left.data < (int) data) {
+
+            root.left = leftRotate(root.left);
+            return rightRotate(root);
+        }
+
+        // Right-Right Case
+        if (balanceFactor < -1 && (int) root.right.data < (int) data) {
+
+            return leftRotate(root);
+        }
+
+        // Right-Left Case
+        if (balanceFactor < -1 && (int) root.right.data > (int) data) {
+
+            root.right = rightRotate(root.right);
+            return leftRotate(root);
         }
 
         return root;
+    }
+
+    private Node leftRotate(Node node) {
+
+        Node left = node.left;
+        Node right = node.right;
+
+        right.left = node;
+        node.right = left;
+
+        node.height = max(getNodeHeight(node.left), getNodeHeight(node.right)) + 1;
+        right.height = max(getNodeHeight(right.left), getNodeHeight(right.right)) + 1;
+
+        return right;
+    }
+
+    private Node rightRotate(Node node) {
+
+        Node left = node.left;
+        Node right = node.right;
+
+        left.right = node;
+        node.left = right;
+
+        node.height = max(getNodeHeight(node.left), getNodeHeight(node.right)) + 1;
+        left.height = max(getNodeHeight(left.left), getNodeHeight(left.right)) + 1;
+
+        return left;
+    }
+
+    // Formula: Balance Factor = Height of Left Subtree - Height of Right Subtree (BF = HL - HR) return -1, 0, 1 based on the balance factor.
+    // If the balance factor is greater than 1 or less than -1, then the tree is unbalanced and we need to perform rotations to balance the tree.
+    private int getBalanceFactor(Node node) {
+
+        if (node == null) {
+
+            return 0;
+        }
+        return getNodeHeight(node.left) - getNodeHeight(node.right);
+    }
+
+    private int max(int nodeLeft, int nodeRight) {
+
+        return Math.max(nodeLeft, nodeRight);
+    }
+
+    private int getNodeHeight(Node node) {
+
+        return node == null ? -1 : node.height;
     }
 
     public boolean contains(T data) {
@@ -182,26 +268,26 @@ public class BinarySearchTreeDS<T> {
 
     public static void main(String[] args) {
 
-        BinarySearchTreeDS<Integer> integerBinarySearchTreeDS = new BinarySearchTreeDS<>(50);
-        integerBinarySearchTreeDS.add(20);
-        integerBinarySearchTreeDS.add(60);
-        integerBinarySearchTreeDS.add(10);
-        integerBinarySearchTreeDS.add(30);
-        integerBinarySearchTreeDS.add(40);
+        AVLTreeDS<Integer> avlTreeDS = new AVLTreeDS<>(50);
+        avlTreeDS.add(20);
+        avlTreeDS.add(60);
+        avlTreeDS.add(10);
+        avlTreeDS.add(30);
+        avlTreeDS.add(40);
 
-        integerBinarySearchTreeDS.displayTreeInOrder();
-        integerBinarySearchTreeDS.displayTree();
+        avlTreeDS.displayTreeInOrder();
+        avlTreeDS.displayTree();
 
-        System.out.println("search = " + integerBinarySearchTreeDS.search(60));
-        System.out.println("contains = " + integerBinarySearchTreeDS.contains(100));
+        System.out.println("search = " + avlTreeDS.search(60));
+        System.out.println("contains = " + avlTreeDS.contains(100));
 
-        System.out.println("search = " + integerBinarySearchTreeDS.search(150));
-        System.out.println("contains = " + integerBinarySearchTreeDS.contains(200));
+        System.out.println("search = " + avlTreeDS.search(150));
+        System.out.println("contains = " + avlTreeDS.contains(200));
 
-        System.out.println("delete = " + integerBinarySearchTreeDS.delete(200));
-        System.out.println("delete = " + integerBinarySearchTreeDS.delete(50));
+        System.out.println("delete = " + avlTreeDS.delete(200));
+        System.out.println("delete = " + avlTreeDS.delete(50));
 
-        integerBinarySearchTreeDS.displayTreeInOrder();
-        integerBinarySearchTreeDS.displayTree();
+        avlTreeDS.displayTreeInOrder();
+        avlTreeDS.displayTree();
     }
 }
